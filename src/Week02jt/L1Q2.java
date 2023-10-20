61a975478f49fd2abcaa337a8589579a25429805
hp <hp@LAPTOP-37R3AJF2>
2023-10-20 15:45:16 +0800

    Q2 DONE.

---
+++ src/Week02jt/L1Q2.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package Week02jt;
+
+/**
+ *
+ * @author Jingtian
+ */
+public class L1Q2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String name="TENG JING TIAN";
+        String matric="22105916";
+        String address="FSKTM UM";
+        String email="jingtianteng@gmail.com";
+        String contact="012-7981180";
+        System.out.println("My Profile");
+        System.out.println("NAME:"+name);
+        System.out.println("MATRIC NUMBER:"+matric);
+        System.out.println("ADDRESS:"+address);
+        System.out.println("EMAIL:"+email);
+        System.out.println("CONTACT:"+contact);
+    }
+    
+}
