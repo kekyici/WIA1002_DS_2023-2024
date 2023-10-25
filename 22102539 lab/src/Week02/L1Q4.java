7f5c59d796ac8fa281f0eca8e6869b2bfb14b717
YiCi Kek <YiCi Kek@YiCiKek>
2023-10-25 16:35:11 +0800

    commit kek

---
+++ 22102539 lab/src/Week02/L1Q4.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package Week02;
+
+/**
+ *
+ * @author YiCi Kek
+ */
+public class L1Q4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+         System.out.println("\n"
+                + " 3200                     *\n"
+                + " 2700                 *   *\n"
+                + " 2500     *           *   *\n"
+                + " 2000     *       *   *   *\n"
+                + " 1600     *   *   *   *   *\n"
+                + " 800      *   *   *   *   *   *\n"
+                + "          *   *   *   *   *   *\n"
+                + "          *   *   *   *   *   *\n"
+                + "          *   *   *   *   *   *\n"
+                + "          *   *   *   *   *   *\n"
+                + "**************************************\n"
+                + "         Jan Feb Mar Apr May Jun");
+    }
+    
+}
