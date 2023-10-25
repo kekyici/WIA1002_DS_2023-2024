4f57c974bcabd1c3336f64512be521e7700048a6
YiCi Kek <YiCi Kek@YiCiKek>
2023-10-25 16:30:55 +0800

    commit kek

---
+++ src/Week02kek/L1Q2.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package Week02kek;
+
+/**
+ *
+ * @author YiCi Kek
+ */
+public class L1Q2 {
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String details = "Full Name: Kek Yi Ci\n"
+                       + "Matric Number: 22102539\n"
+                       +"Address: FSKTM UM \n"
+                       +"Email: kekyici25@gmail.com\n"
+                       +"Contanct: 011-51997123";
+        System.out.println(details);
+                
+    }
+    
+}
---
+++ src/Week02kek/L1Q3.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package Week02kek;
+
+/**
+ *
+ * @author YiCi Kek
+ */
+public class L1Q3 {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        System.out.println("**********             ***              *           *           \n"
+                         + "*        *          *       *          ***        *   *         \n"
+                         + "*        *        *          *        *****      *     *        \n"
+                         + "*        *        *          *          *       *       *       \n"
+                         + "*        *        *          *          *      *         *      \n"
+                         + "*        *        *          *          *       *       *       \n"
+                         + "*        *        *          *          *        *     *        \n"
+                         + "*        *          *       *           *         *   *          \n"
+                         + "**********             ***              *           *            ");
+                
+        
+        
+        
+    }
+    
+}
